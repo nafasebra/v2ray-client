be7bcf91a76a006689b9ef16e6a27fbff7317f48
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2023-11-24 14:54:51 +0330

    feat: add default options to tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import RouterElement from "@/router/RouterElement";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (

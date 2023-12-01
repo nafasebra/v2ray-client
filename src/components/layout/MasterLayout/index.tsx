f92217169f5dc0f1f3ed7f031c423d5cccaf49b8
Nafas Ebrahimi <ebranafas@gmail.com>
2023-12-01 21:44:50 +0330

    perf: remove container component

--- src/components/layout/MasterLayout/index.tsx
+++ src/components/layout/MasterLayout/index.tsx
@@ -1,22 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
-import Container from "./Container";
 
-import type { ReactNode } from "react";
-
-export interface MasterLayoutProps {
-  children?: ReactNode;
-}
-
-export default function MasterLayout(props: MasterLayoutProps) {
+export default function MasterLayout() {
   return (
-    <Container>
+    <div className="container_app">
       <Header />
       <main className="text-white font-Poppins rtl:font-Morabba">
-        {props.children || <Outlet />}
+        {<Outlet />}
       </main>
       <Footer />
-    </Container>
+    </div>
   );
 }

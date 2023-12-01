ee9f18f719a2e8bb80332bcc759857152fcc92a1
Nafas Ebrahimi <ebranafas@gmail.com>
2023-12-01 22:23:39 +0330

    feat: complete transition

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,8 +1,11 @@
 import ActiveChart from "@/components/pice/ActiveChart";
 import QRCodeContainer from "@/components/pice/QRCodeContainer";
 import AppLinksSection from "@/components/status/AppLinksSection";
+import { useTranslation } from "react-i18next";
 
 function Details() {
+  const { t } = useTranslation();
+
   return (
     <section className="container mx-auto flex flex-col lg:flex-row gap-20 min-h-[calc(100vh-100px)] p-6">
       <aside className="w-full lg:w-[20%] space-y-3">
@@ -12,49 +15,61 @@ function Details() {
         <QRCodeContainer valueQrCode="thisissaman" />
         <div className="grid grid-cols-2 gap-3">
           <button className="rounded-full py-2 px-6 font-bold text-black gradient">
-            Copy
+            {t("details.button.copy")}
           </button>
           <button className="rounded-full py-2 px-6 font-bold text-black gradient">
-            Change
+            {t("details.button.change")}
           </button>
         </div>
       </aside>
       <article className="w-full lg:w-[60%]">
         <div className="space-y-5 border border-white rounded-lg p-3">
           <h2 className="text-center text-3xl font-bold gradient text-transparent bg-clip-text">
-            Traffic
+            {t("details.traffic")}
           </h2>
           <div className="grid grid-cols-2 gap-2">
             <div className="flex items-center">
-              <p className="font-bold gradient bg-clip-text text-transparent">Active</p>
+              <p className="font-bold gradient bg-clip-text text-transparent">
+                {t("details.status.active")}
+              </p>
             </div>
             <ActiveChart />
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
             <div className="space-y-3">
               <div className="grid grid-cols-2 gap-2">
-                <p className="font-bold gradient bg-clip-text text-transparent">Expire</p>
+                <p className="font-bold gradient bg-clip-text text-transparent">
+                  {t("details.status.expire")}
+                </p>
               </div>
               <div className="grid grid-cols-2 gap-2">
-                <p className="font-bold gradient bg-clip-text text-transparent">Total Traffic</p>
+                <p className="font-bold gradient bg-clip-text text-transparent">
+                  {t("details.status.totalTraffic")}
+                </p>
               </div>
               <div className="grid grid-cols-2 gap-2">
-                <p className="font-bold gradient bg-clip-text text-transparent">Total Usage</p>
+                <p className="font-bold gradient bg-clip-text text-transparent">
+                  {t("details.status.totalUsage")}
+                </p>
               </div>
               <div className="grid grid-cols-2 gap-2">
-                <p className="font-bold gradient bg-clip-text text-transparent">Download</p>
+                <p className="font-bold gradient bg-clip-text text-transparent">
+                  {t("details.status.download")}
+                </p>
               </div>
               <div className="grid grid-cols-2 gap-2">
-                <p className="font-bold gradient bg-clip-text text-transparent">Upload</p>
+                <p className="font-bold gradient bg-clip-text text-transparent">
+                  {t("details.status.upload")}
+                </p>
               </div>
             </div>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
             <button className="rounded-full py-2 px-6 font-bold text-black gradient">
-              Copy status
+              {t("details.button.copyStatus")}
             </button>
             <button className="rounded-full py-2 px-6 font-bold text-black gradient">
-              Share status
+              {t("details.button.shareStatus")}
             </button>
           </div>
         </div>

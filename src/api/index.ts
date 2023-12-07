9a12ca7f422c31885e33701f28762cf0bc80d719
Nafas Ebrahimi <ebranafas@gmail.com>
2023-12-07 13:18:11 +0330

    fix: change api link

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://beta-patrick-stats.ilyagvc.online/PatrickStats",
+  baseURL: "https://beta.vip-status.site/PatrickStats",
 });
